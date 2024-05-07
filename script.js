b32f47db8f19b9314e4edd152e729ea243050b13
Elliot <ellthrowaway2@icloud.com>
2024-05-07 14:05:28 -0400

    date formatter lesson complete

--- script.js
+++ script.js
@@ -14,5 +14,11 @@ dateOptionsSelectElement.addEventListener("change", () => {
         case 'yyyy-mm-dd':
             currentDateParagraph.textContent = formattedDate
             formattedDate.split("-").reverse().join("-")
+            break;
+        case 'mm-dd-yyyy-h-mm':
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`
+            break;
+        default:
+            currentDateParagraph.textContent = formattedDate
     }
 })
